fca9fd78a199b1ef39fdba0d921ac8d61ff3e928
AbdulManan00 <abdulmanan033642@gmail.com>
2024-05-08 15:37:53 +0500

    added a dom.html file

--- test.js
+++ test.js
@@ -1,27 +1,26 @@
-let a ='manan'
-var b='heeloo'
-const c ='nice one'
-let d='10'
-let e=Number(d)
+let a = "manan";
+var b = "heeloo";
+const c = "nice one";
+let d = "10";
+let e = Number(d);
 // console.log(typeof e);
-const heroes=["batman","superman","spiderman"]
-const heroes2={
-    "batman":"hero",
-    "superman":"hero",
-    "spiderman":"hero"
-}
+const heroes = ["batman", "superman", "spiderman"];
+const heroes2 = {
+	batman: "hero",
+	superman: "hero",
+	spiderman: "hero",
+};
 const myFunction = function () {
-    console.table([heroes]);
-    
-}
+	console.table([heroes]);
+};
 // myFunction()
 // let name= new String("manan")
 // console.log(name);
-let name ={
-    name:'manan',
-}
-let name2=name;
- name2.name='Asif';
+let name = {
+	name: "manan",
+};
+let name2 = name;
+name2.name = "Asif";
 // console.table([name,name2]);
 let myDate = new Date();
 // console.table([myDate.toDateString(),myDate.toJSON()]);
@@ -29,16 +28,79 @@ let myDate2 = new Date("08/01/2000");
 // console.log(myDate2.toLocaleString());
 // console.log(Date.now());
 // console.log(myDate.toLocaleString('default',{timeZone:'Asia/Kolkata'}));
-let arr=[1,2,3,4,5]
-let arr1=[6,7,8,9,10]
-let arr2=[...arr,...arr1]
+let arr = [1, 2, 3, 4, 5];
+let arr1 = [6, 7, 8, 9, 10];
+let arr2 = [...arr, ...arr1];
 // console.log(arr2);
-let year=Symbol(3000);
-let obj={
-    name:'manan',
-    age:20,
-[year]:year
-}
+let year = Symbol(3000);
+let obj = {
+	name: "manan",
+	age: 20,
+	[year]: year,
+};
 // console.log(obj.name);
 // console.log(obj['age']);
-console.log(typeof obj[year]);
+// console.log(typeof obj[year]);
+obj.greeting = function () {
+	console.log("hello");
+};
+obj.greeting2 = function () {
+	console.log(`hello ${this.name} ${this.age}`);
+};
+// console.log(obj.greeting());
+// console.log(obj.greeting2());
+// console.log(Object.entries(obj));
+// console.log(obj.hasOwnProperty('greeting2'));
+// console.log(2==='2');
+const addTwo = () => {
+	return 2 + 2;
+};
+// console.log(addTwo())
+let obj2 = {
+	name: "manan",
+	age: 20,
+	myFunction: function () {
+		console.log(`Hello ${this.name}!`);
+	},
+};
+// console.log(obj2.myFunction());
+// ((name)=>{
+//   console.log("Inner anonymous function called "+name);
+// })('manan');
+// ((name)=>{
+//   console.log("outer anonymous function called "+name);
+// })('manan');
+let obj3 = {
+	name: "john",
+	age: 25,
+};
+
+// Spread the object properties into an array
+let arr3 = ["manan", "heeloo", ...Object.values(obj3)];
+// console.log(arr3);
+
+let arr4 = ["a", "b", "c"];
+arr4.forEach(function (val) {
+	// console.log(val)
+});
+let arr5 = ["a", "b", "c"];
+arr5.forEach((nn, i, c) => {
+	console.log(nn + i, c);
+});
+let arr6 = [
+	{ name: "manan", age: 20 },
+	{ name: "heeloo", age: 30 },
+	{ name: "nice one", age: 40 ,num:33},
+];
+
+arr6.forEach(( item ) => {
+	console.log(item.num);
+});
+
+// (function n(){
+// console.log('object');
+// })();
+
+// (()=>{
+//     console.log('object');
+//     })()
