acbedc037bb5ce6ca26dd131852405c60c13f8cc
AbdulManan00 <abdulmanan033642@gmail.com>
2024-05-09 16:19:59 +0500

    added a dom.js file

---
+++ dom.js
@@ -0,0 +1,4 @@
+document.querySelector("ul li:nth-child(3)").style.backgroundColor = "red";
+document.querySelector('.change').style.backgroundColor='orange';
+document.getElementById("change-color").style.color = "blue";
+console.log(document.querySelector("#change-color"));
